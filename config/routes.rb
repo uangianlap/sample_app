259cf00fdfc4a8ef579149633577f6dcc84f4f4a
uangianlap <huangjian@protonmail.com>
2018-02-27 19:11:45 +0800

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
 
   get 'static_pages/help'
 
+	get 'static_pages/about'
+
 	get '/hello', to: 'application#hello'
 	get '/goodbye', to: 'application#goodbye'
-  root 'application#index'
+
+  root 'static_pages#home'
 end
