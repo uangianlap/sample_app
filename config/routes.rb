b3e2d6e544f24f2dbb24506a7848dabff42af406
uangianlap <huangjian@protonmail.com>
2018-02-27 21:20:40 +0800

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  get 'users/new'
 
-  get 'static_pages/help'
-
-	get 'static_pages/about'
+  get 'home' => 'static_pages#home'
+  get 'help' => 'static_pages#help'
+	get 'about' => 'static_pages#about'
+	get 'contact' => 'static_pages#contact'
+	get 'singup'  => 'users#new'
 
 	get '/hello', to: 'application#hello'
 	get '/goodbye', to: 'application#goodbye'
