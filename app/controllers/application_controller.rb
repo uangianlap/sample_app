03ea6205430d43f465036f7aa160459685d217f2
uangianlap <huangjian@protonmail.com>
2018-02-27 17:47:02 +0800

    change index action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+	
+	def index
+		render :text => "It's a good day? isn't it?"
+	end
 
 	def hello
-		render :text => "Hello, world!"
+		render :text => "Hello, Huangjian!"
 	end
 
 	def goodbye
-		render :text => "Goodbye, world!"
+		render :text => "Goodbye, Huangjian!"
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  root 'application#goodbye'
+	get '/hello', to: 'application#hello'
+	get '/goodbye', to: 'application#goodbye'
+  root 'application#index'
 end
